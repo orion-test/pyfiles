7ed4b69b3fa51b2f544a7602af49cb7e9d52cedf
Vivien Lok <vivien.lok@gmail.com>
2016-06-15 11:03:35 -0700

    updated values

--- int1.py
+++ int1.py
@@ -4,3 +4,9 @@ num2 = -84
 print ("Type of first number is ", type (num1))
 print ("Type of second number is ", type (num2))
 
+num1 = 19.1
+num2 = -18.9
+
+print ("Type of first number is ", type (num1))
+print ("Type of second number is ", type (num2))
+
