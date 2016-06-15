faa289c59b6ccf487ddbb5dffd9b1199f4c7ff8f
Vivien Lok <vivien.lok@gmail.com>
2016-06-15 15:47:41 -0700

    add strings file

---
+++ str1.py
@@ -0,0 +1,17 @@
+sample1 = 'This is a single quote example'
+sample2 = "This is a double quote example"
+sample3 = '''This is a triple single quote example'''
+sample4 = """This is a triple double quote example"""
+multiline = """This is the firstline
+This is the second line
+This is the third line"""
+
+print ((sample1))
+print ((sample2))
+print ((sample3))
+print ((sample4))
+print ((multiline))
+print(type (sample1))
+print(type (sample2))
+print(type (sample3))
+print(type (sample4))
